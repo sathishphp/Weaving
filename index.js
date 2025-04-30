cc6a03a2e573ef98ba6b3e679b648494bd654e30
Sathishkumar Ramasamy <sathishkumar.r@actian.com>
2025-04-30 12:58:07 +0530

    testing setup

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const mysql = require('mysql2');
+const app = express();
+const PORT = 3000;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// MySQL Connection
+const db = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'test'
+});
+
+// Connect to MySQL
+db.connect(err => {
+    if (err) throw err;
+    console.log('Connected to MySQL Database');
+});
+
+// Create a new user
+app.post('/users', (req, res) => {
+    const { name, email } = req.body;
+    const query = 'INSERT INTO users (name, email) VALUES (?, ?)';
+    db.query(query, [name, email], (err, result) => {
+        if (err) return res.status(500).send(err);
+        res.status(201).send({ id: result.insertId, name, email });
+    });
+});
+
+// Read all users
+app.get('/users', (req, res) => {
+    const query = 'SELECT * FROM users';
+    db.query(query, (err, results) => {
+        if (err) return res.status(500).send(err);
+        res.status(200).json(results);
+    });
+});
+
+// Update a user
+app.put('/users/:id', (req, res) => {
+    const { id } = req.params;
+    const { name, email } = req.body;
+    const query = 'UPDATE users SET name = ?, email = ? WHERE id = ?';
+    db.query(query, [name, email, id], (err, result) => {
+        if (err) return res.status(500).send(err);
+        res.status(200).send({ id, name, email });
+    });
+});
+
+// Delete a user
+app.delete('/users/:id', (req, res) => {
+    const { id } = req.params;
+    const query = 'DELETE FROM users WHERE id = ?';
+    db.query(query, [id], (err, result) => {
+        if (err) return res.status(500).send(err);
+        res.status(204).send();
+    });
+});
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
